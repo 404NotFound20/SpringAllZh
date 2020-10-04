d209377094d53881eebf6d77200890607ecd536c
404NotFound20 <1376041660@qq.com>
2020-10-04 18:38:30 +0800

    first commit

---
+++ modle-test/src/main/java/com/zh/modletest/ModleTestApplication.java
@@ -0,0 +1,13 @@
+package com.zh.modletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModleTestApplication.class, args);
+	}
+
+}
---
+++ modle-test/src/test/java/com/zh/modletest/ModleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zh.modletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
