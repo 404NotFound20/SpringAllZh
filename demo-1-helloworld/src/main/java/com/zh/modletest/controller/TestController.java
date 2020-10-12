3b426695906ca6bad06ffbf52575b90cf2b36388
404NotFound20 <1376041660@qq.com>
2020-10-12 23:47:46 +0800

    {test demo} 两张方式配置获取

--- demo-1-helloworld/src/main/java/com/zh/modletest/controller/TestController.java
+++ demo-1-helloworld/src/main/java/com/zh/modletest/controller/TestController.java
@@ -1,12 +1,32 @@
 package com.zh.modletest.controller;
 
+import com.zh.modletest.property.ApplicationProperty;
+import com.zh.modletest.property.DeveloperProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 public class TestController {
+    private final ApplicationProperty applicationProperty;
+    private final DeveloperProperty developerProperty;
+
+    public TestController(ApplicationProperty applicationProperty, DeveloperProperty developerProperty) {
+        this.applicationProperty = applicationProperty;
+        this.developerProperty = developerProperty;
+    }
+
+
     @GetMapping("/hello")
     public  String seyHello(){
         return "hello World";
+    }
+    @GetMapping("/property")
+    public HashMap<String,Object> getProperty(){
+        HashMap<String,Object> map=new HashMap<>();
+        map.put("applicationProperty",applicationProperty);
+        map.put("developerProperty",developerProperty);
+        return map;
     }
 }
--- demo-1-helloworld/src/main/java/com/zh/modletest/property/DeveloperProperty.java
+++ demo-1-helloworld/src/main/java/com/zh/modletest/property/DeveloperProperty.java
@@ -1,8 +1,12 @@
 package com.zh.modletest.property;
 
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
+@ConfigurationProperties(prefix = "developer")
 public class DeveloperProperty {
     private String name;
     private String website;
